6562ebf25c923df53855e76c5c54bc77af6b38ca
peeyush singla <peeyush.singla@gmail.com>
2019-07-22 15:36:20 +0200

    2nd commit

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import {reduce} from 'lodash';
+
+
+let numRef = [
+  {
+    "num": 1,
+    "word": "One"
+  },
+  {
+    "num": 2,
+    "word": "Two"
+  },
+  {
+    "num": 3,
+    "word": "Three"
+  },
+  {
+    "num": 4,
+    "word": "Four"
+  },
+  {
+    "num": 5,
+    "word": "Five"
+  },
+  {
+    "num": 0,
+    "word": "Zero"
+  }
+]
+
+
+export function numToWord(num: number) {
+  return reduce(numRef, (accum, ref) => {
+    return ref.num === num ? ref.word : accum;
+  }, '');
+}
+
+export function wordToNum(word: string) {
+  return reduce(numRef, (accum, ref) => {
+    return ref.word === word && word.toLowerCase() ? ref.num : accum;
+  }, -1);
+}
